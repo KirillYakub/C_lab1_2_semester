8f4159b5dddb0910a192078f3da4d39bfb7be06e
KirillYakub <kirillyakubchinskiy@gmail.com>
2020-03-07 14:08:43 +0300

    Улучшен код задания уровня "medium"

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -17,11 +18,11 @@ void easy()
 		int inform;
 	};
 
-    const int n = 3;
+    const int N = 2;
 
-	students number[n];
+	students number[N];
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < N; i++)
 	{
 		cout << "Студент по номером " << i + 1 << endl;
 		cout << "Введите фамилию: ";
@@ -50,11 +51,12 @@ void easy()
 		cout << endl << endl;
 	}
 
+	//создаем счетчик и цикл с условием для подсчета студентов с баллом по математике = 95 и выводим их фамилии;
 	int count = 0;
 
 	cout << "Студенты, сдавшие математику на 95: ";
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < N; i++)
 	{
 		if (number[i].math == 95)
 		{
@@ -69,40 +71,77 @@ void easy()
 
 }
 
+
 void medium()
 {
 
 	cout << "Вывести данные об авиарейсе с максимальной длительностью полета" << endl << endl;
 
+	//создаем структуры со временем вылета и прилета;
+	struct time_depart
+	{
+		int hours_depart;
+		int minutes_depart;
+	};
+
+	struct time_arrive
+	{
+		int hours_arrive;
+		int minutes_arrive;
+	};
+
 	struct avia
 	{
 		int number;
-		double depart;
-		double arrive;
+		time_depart depart;
+		time_arrive arrive;
 		char direct[32];
 		char mark[32];
 		int distance;
-		double flight_time;
+		int fly_time;
+		int fly_time_hours;
+		int fly_time_minutes;
     };
 
-	const int n = 3;
+	//создаем массив и цикл для последовательного ввода значений элементов структуры;
+	const int N = 2;
 
-	avia number[n];
+	avia number[N];
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < N; i++)
 	{
-		cout << "Рейс по номером " << i + 1 << endl;
+		
 		cout << "Введите номер: ";
 		cin >> number[i].number;
 		cout << endl;
 
-		cout << "Введите время вылета: ";
-		cin >> number[i].depart;
+		do
+		{
+			cout << "Введите время вылета в часах: ";
+			cin >> number[i].depart.hours_depart;
+		} while (number[i].depart.hours_depart > 23 || number[i].depart.hours_depart < 0);
 		cout << endl;
 
-		cout << "Введите время прилета: ";
-		cin >> number[i].arrive;
+		do
+		{
+			cout << "Введите время вылета в минутах: ";
+			cin >> number[i].depart.minutes_depart;
+		} while (number[i].depart.minutes_depart > 59 || number[i].depart.minutes_depart < 0);
 		cout << endl;
+			
+		do
+		{
+			cout << "Введите время прилета в часах: ";
+			cin >> number[i].arrive.hours_arrive;
+		} while (number[i].arrive.hours_arrive > 23 || number[i].arrive.hours_arrive < 0);
+		cout << endl;
+
+		do
+		{
+			cout << "Введите время прилета в минутах: ";
+			cin >> number[i].arrive.minutes_arrive;
+		} while (number[i].arrive.minutes_arrive > 59 || number[i].arrive.minutes_arrive < 0);
+	    cout << endl;
 
 		cout << "Направление: ";
 		cin.ignore(cin.rdbuf()->in_avail());
@@ -118,29 +157,51 @@ void medium()
 		cin >> number[i].distance;
 		cout << endl << endl;
 
-		number[i].flight_time = abs(number[i].arrive - number[i].depart);
-    }
+		//переводим часы в минуты и находим время полета;
+		int hour_in_minutes_depart = number[i].depart.hours_depart*60;
+		hour_in_minutes_depart = hour_in_minutes_depart + number[i].depart.minutes_depart;
 
-	cout << endl << endl;
+		int hour_in_minutes_arrive = number[i].arrive.hours_arrive * 60;
+		hour_in_minutes_arrive = hour_in_minutes_arrive + number[i].arrive.minutes_arrive;
 
-	short max_flight_time = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		if (number[i].flight_time > number[max_flight_time].flight_time)
+		if (hour_in_minutes_arrive >= hour_in_minutes_depart)
 		{
-			max_flight_time = i;
+			number[i].fly_time = hour_in_minutes_arrive - hour_in_minutes_depart;
+		}
+
+		if (hour_in_minutes_arrive < hour_in_minutes_depart)
+		{
+			number[i].fly_time = (24 * 60) - (hour_in_minutes_depart - hour_in_minutes_arrive);
 		}
 	}
 
+	int max_fligth_time = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+        if (number[i].fly_time > number[max_fligth_time].fly_time)
+		{
+			max_fligth_time = i;
+		}
+	}
+
+	//переводим минуты обратно в часы;
+	for (int i = 0; i < N; i++)
+	{
+		number[i].fly_time_hours = number[i].fly_time/60;
+		number[i].fly_time_minutes = number[i].fly_time % 60;
+	}
+
 	cout << "Информация о рейсе с найбольшей длительностью полета:" << endl;
-	cout << "Номер рейса: " << number[max_flight_time].number << endl;
-	cout << "Время вылета: " << number[max_flight_time].depart << endl;
-	cout << "Время прилета: " << number[max_flight_time].arrive << endl;
-	cout << "Направление: " << number[max_flight_time].direct << endl;
-	cout << "Марка самолета: " << number[max_flight_time].mark << endl;
-	cout << "Время полета: " << number[max_flight_time].flight_time << endl;
+	cout << "Номер рейса: " << number[max_fligth_time].number << endl;
+	cout << "Время вылета: " << number[max_fligth_time].depart.hours_depart << ":" << number[max_fligth_time].depart.minutes_depart << endl;
+	cout << "Время прилета: " << number[max_fligth_time].arrive.hours_arrive << ":" << number[max_fligth_time].arrive.minutes_arrive << endl;
+	cout << "Марка: " << number[max_fligth_time].mark << endl;
+	cout << "Дистанция: " << number[max_fligth_time].distance << endl;
+	cout << "Время полета: " << number[max_fligth_time].fly_time_hours << ":" << number[max_fligth_time].fly_time_minutes << endl << endl;
 }
+
+
 
 int main()
 {
@@ -158,9 +219,10 @@ int main()
 	{
 		easy();
 	}
-	else 
+	
+	if(number == 2)
 	{
 		medium();
 	}
-	
+
 }
